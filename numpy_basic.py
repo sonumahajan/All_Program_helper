31c33e41f0f2fa89a9bd340f9a8983077d71698d
Pramdhanni <65021963+bimbims125@users.noreply.github.com>
2021-10-03 07:53:29 +0700

    add file

---
+++ numpy_basic.py
@@ -0,0 +1,41 @@
+# ============= WHAT IS NUMPY ? =============
+
+# NumPy is the fundamental package for scientific computing in Python. It is a Python library that provides 
+# a multidimensional array object, various derived objects (such as masked arrays and matrices), and an assortment 
+# of routines for fast operations on arrays, 
+# including mathematical, logical, shape manipulation, sorting, selecting, I/O, discrete Fourier transforms, 
+# basic linear algebra, basic statistical operations, 
+# random simulation and much more.
+
+
+
+
+#  ============= REQUIREMENT =============
+# pip install numpy
+
+# ============= IMPORT MODULE/PACKAGE =============
+import numpy as np
+
+# ============= MAKE ARRAY USING NUMPY =============
+array1 = np.array([1, 2, 3 ,4, 5])
+print("Result is :" , array1)
+
+# RESULT :
+    #  Result is : [1 2 3 4 5]
+
+# ============= MAKE ARRAY WITH 0 VALUE =============
+array2 = np.zeros(5) # MAKE ARRAY 0 VALUE AS MUCH 5
+print("Result is :", array2)
+
+# RESULT :
+    #  Result is : [0. 0. 0. 0. 0.]
+
+# ============= MAKE ARRAY WITH 1 VALUE =============
+array3 = np.ones(5) # MAKE ARRAY 1 VALUE AS MUCH 5
+print("Result is :", array3)
+
+# RESULT :
+    #  Result is : [1. 1. 1. 1. 1.]
+    
+    
+# ==================== THANKS FOR YOUR ATTENTION, I HOPE YOU LIKE THIS ====================
