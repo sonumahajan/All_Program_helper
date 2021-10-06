36b64fab855e9a6e750815e54851e5c4f8c90d9d
AbinashSamal828 <82222743+AbinashSamal828@users.noreply.github.com>
2021-10-07 01:00:07 +0530

    Check pallindrome simple c program

---
+++ pallindrome.c
@@ -0,0 +1,29 @@
+/*Write a program that will check the given string is palindrome or not.
+Name :Abinash Samal Group:C1 Roll No.:02*/
+#include<stdio.h>
+int main()
+{
+    char str[30],temp;
+    int j=0,i=0,mid,flag=0;
+    printf("Enter the string to be checked for pallindrome:\n");
+    gets(str);
+    while(str[j]!='\0')
+        j++;
+    mid=j/2;
+    while(i<mid)
+    {
+        j--;
+        if(str[i]!=str[j])
+        {
+            flag=1;
+            break;
+        }
+        i++;
+    }
+    if(flag==0)
+        printf("It is a pallindrome.");
+    else
+        printf("It is not a pallindrome.");
+    return 0;
+
+}
