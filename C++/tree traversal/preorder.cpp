af055a43076ba2aeba04cb19e0436f616f5891f4
Taniya <65013132+taniya76@users.noreply.github.com>
2021-10-07 16:44:53 +0530

    create preorder travesal.cpp

---
+++ C++/tree traversal/preorder.cpp
@@ -0,0 +1,19 @@
+/* Algorithm Preorder(tree)
+   1. Visit the root.
+   2. Traverse the left subtree, i.e., call Preorder(left-subtree)
+   3. Traverse the right subtree, i.e., call Preorder(right-subtree)
+*/
+void printPreorder(struct Node* node)
+{
+    if (node == NULL)
+        return;
+ 
+    /* first print data of node */
+    cout << node->data << " ";
+ 
+    /* then recur on left subtree */
+    printPreorder(node->left);
+ 
+    /* now recur on right subtree */
+    printPreorder(node->right);
+}
