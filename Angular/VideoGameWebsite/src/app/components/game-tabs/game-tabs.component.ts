5fe5b73db5dd2428f3cc5757a2692fd77bf18293
KedarisettiSreeVamsi <ksreevamsi32@gmail.com>
2021-10-17 11:45:34 +0530

    Angular Application

---
+++ Angular/VideoGameWebsite/src/app/components/game-tabs/game-tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
+
+@Component({
+  selector: 'app-game-tabs',
+  templateUrl: './game-tabs.component.html',
+  styleUrls: ['./game-tabs.component.scss']
+})
+export class GameTabsComponent implements OnInit {
+  @Input() game!: Game;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/VideoGameWebsite/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com'
+};
