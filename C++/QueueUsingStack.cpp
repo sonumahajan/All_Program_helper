14ad3dfe7116f3ae9618381c3be818c3801979ec
Akanksha Singh <79742074+akanksha57@users.noreply.github.com>
2021-10-18 22:10:51 +0530

    Update QueueUsingStack.cpp

--- C++/QueueUsingStack.cpp
+++ C++/QueueUsingStack.cpp
@@ -1,56 +1,56 @@
-#include <iostream>
-#include <stack>
+// CPP program to implement Queue using
+// two stacks with costly enQueue()
+#include <bits/stdc++.h>
 using namespace std;
 
-class queue{
-    stack<int> s1;
-    stack<int> s2;
-    //two stacks are used
-    //one stores the data in lifo model
-    //another one starts storing elements of s1
-    //when pop function is used on queue, elements of s2 are displayed
+struct Queue {
+	stack<int> s1, s2;
 
-    public:
-    void push(int x){
-        s1.push(x);
-    }
-    int pop(){
-        if(s1.empty() && s2.empty()){
-            cout<<"Queue is empty!"<<endl;
-            return -1;
-        }
-        if(s2.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        int topval = s2.top();
-        s2.pop();
-        return topval;
-    }
+	void enQueue(int x)
+	{
+		// Move all elements from s1 to s2
+		while (!s1.empty()) {
+			s2.push(s1.top());
+			s1.pop();
+		}
 
-    bool empty() {
-        if(s1.empty() && s2.empty()){
-            return true;
-        }
-        return false;
-    }
+		// Push item into s1
+		s1.push(x);
+
+		// Push everything back to s1
+		while (!s2.empty()) {
+			s1.push(s2.top());
+			s2.pop();
+		}
+	}
+
+	// Dequeue an item from the queue
+	int deQueue()
+	{
+		// if first stack is empty
+		if (s1.empty()) {
+			cout << "Q is Empty";
+			exit(0);
+		}
+
+		// Return top of s1
+		int x = s1.top();
+		s1.pop();
+		return x;
+	}
 };
 
+// Driver code
 int main()
 {
-    queue q;
-    q.push(1);
-    q.push(2);
-    q.push(3);
-    q.push(4);
-    cout<<q.pop()<<endl;
-    q.push(5);
-    cout<<q.pop()<<endl;
-    cout<<q.pop()<<endl;
-    cout<<q.pop()<<endl;
-    cout<<q.pop()<<endl;
-    cout<<q.pop()<<endl;
-    cout<<q.empty()<<endl;
-    
-    return 0;
+	Queue q;
+	q.enQueue(1);
+	q.enQueue(2);
+	q.enQueue(3);
+
+	cout << q.deQueue() << '\n';
+	cout << q.deQueue() << '\n';
+	cout << q.deQueue() << '\n';
+
+	return 0;
 }
