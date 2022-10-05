40d6e4726e82a0a35576f310171f857ea6662802
GhulamID <dubay887@gmail.com>
2022-10-05 12:16:07 +0700

    add median.js at javascript folder

---
+++ Javascript/median.js
@@ -0,0 +1,12 @@
+function median(numbers) {
+    const sorted = Array.from(numbers).sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    }
+
+    return sorted[middle];
+}
+
+console.log(median([4, 5, 7, 1, 33]));
