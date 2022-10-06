6ccfd85f0a6a63cee71019db0b0c222308494bf3
Shubham Kumar <92396674+shubham-bit2@users.noreply.github.com>
2022-10-06 20:59:16 +0530

    Add files via upload

---
+++ web-projects/Blog4Techie/frontend/components/Book/BookDetail.js
@@ -0,0 +1,132 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormLabel,
+  TextField,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const BookDetail = () => {
+  const [inputs, setInputs] = useState();
+  const id = useParams().id;
+  const [checked, setChecked] = useState(false);
+  const history = useNavigate();
+  useEffect(() => {
+    const fetchHandler = async () => {
+      await axios
+        .get(`https://the-web-book-store.herokuapp.com/books/${id}`)
+        .then((res) => res.data)
+        .then((data) => setInputs(data.book));
+    };
+    fetchHandler();
+  }, [id]);
+
+  const sendRequest = async () => {
+    await axios
+      .put(`https://the-web-book-store.herokuapp.com/books/${id}`, {
+        name: String(inputs.name),
+        author: String(inputs.author),
+        description: String(inputs.description),
+        price: Number(inputs.price),
+        image: String(inputs.image),
+        available: Boolean(checked),
+      })
+      .then((res) => res.data);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => history("/books"));
+  };
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <div>
+      {inputs && (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent={"center"}
+            maxWidth={700}
+            alignContent={"center"}
+            alignSelf="center"
+            marginLeft={"auto"}
+            marginRight="auto"
+            marginTop={10}
+          >
+            <FormLabel>Name</FormLabel>
+            <TextField
+              value={inputs.name}
+              onChange={handleChange}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              name="name"
+            />
+            <FormLabel>Author</FormLabel>
+            <TextField
+              value={inputs.author}
+              onChange={handleChange}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              name="author"
+            />
+            <FormLabel>Description</FormLabel>
+            <TextField
+              value={inputs.description}
+              onChange={handleChange}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              name="description"
+            />
+            <FormLabel>Price</FormLabel>
+            <TextField
+              value={inputs.price}
+              onChange={handleChange}
+              type="number"
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              name="price"
+            />
+            <FormLabel>Image</FormLabel>
+            <TextField
+              value={inputs.image}
+              onChange={handleChange}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              name="image"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={checked}
+                  onChange={() => setChecked(!checked)}
+                />
+              }
+              label="Available"
+            />
+
+            <Button variant="contained" type="submit">
+              Update Book
+            </Button>
+          </Box>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default BookDetail;
