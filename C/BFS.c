ad36823355465f08ed4afe2bf1f73a9677576c54
Parth Dwivedi <parthdwivedi2002@gmail.com>
2023-10-19 05:21:28 +0530

    Update BFS.c

--- C/BFS.c
+++ C/BFS.c
@@ -1,127 +1,34 @@
 #include<stdio.h>
-#include<stdlib.h>
- 
-#define MAX 100  
- 
-#define initial 1
-#define waiting 2
-#define visited 3
- 
-int n;    
-int adj[MAX][MAX];
-int state[MAX]; 
-void create_graph();
-void BF_Traversal();
-void BFS(int v);
- 
-int queue[MAX], front = -1,rear = -1;
-void insert_queue(int vertex);
-int delete_queue();
-int isEmpty_queue();
- 
-int main()
-{
-	create_graph();
-	BF_Traversal();
-	return 0;
+#include<conio.h>
+int a[20][20],q[20],visited[20],n,i,j,f=0,r=-1;
+void bfs(int v) {
+	for (i=1;i<=n;i++)
+	  if(a[v][i] && !visited[i])
+	   q[++r]=i;
+	if(f<=r) {
+		visited[q[f]]=1;
+		bfs(q[f++]);
+	}
 }
- 
-void BF_Traversal()
-{
+void main() {
 	int v;
-	
-	for(v=0; v<n; v++) 
-		state[v] = initial;
-	
-	printf("Enter Start Vertex for BFS: \n");
-	scanf("%d", &v);
-	BFS(v);
-}
- 
-void BFS(int v)
-{
-	int i;
-	
-	insert_queue(v);
-	state[v] = waiting;
-	
-	while(!isEmpty_queue())
-	{
-		v = delete_queue( );
-		printf("%d ",v);
-		state[v] = visited;
-		
-		for(i=0; i<n; i++)
-		{
-			if(adj[v][i] == 1 && state[i] == initial) 
-			{
-				insert_queue(i);
-				state[i] = waiting;
-			}
-		}
-	}
-	printf("\n");
-}
- 
-void insert_queue(int vertex)
-{
-	if(rear == MAX-1)
-		printf("Queue Overflow\n");
-	else
-	{
-		if(front == -1) 
-			front = 0;
-		rear = rear+1;
-		queue[rear] = vertex ;
-	}
-}
- 
-int isEmpty_queue()
-{
-	if(front == -1 || front > rear)
-		return 1;
-	else
-		return 0;
-}
- 
-int delete_queue()
-{
-	int delete_item;
-	if(front == -1 || front > rear)
-	{
-		printf("Queue Underflow\n");
-		exit(1);
-	}
-	
-	delete_item = queue[front];
-	front = front+1;
-	return delete_item;
-}
- 
-void create_graph()
-{
-	int count,max_edge,origin,destin;
- 
-	printf("Enter number of vertices : ");
+	printf("\n Enter the number of vertices:");
 	scanf("%d",&n);
-	max_edge = n*(n-1);
- 
-	for(count=1; count<=max_edge; count++)
-	{
-		printf("Enter edge %d( -1 -1 to quit ) : ",count);
-		scanf("%d %d",&origin,&destin);
- 
-		if((origin == -1) && (destin == -1))
-			break;
- 
-		if(origin>=n || destin>=n || origin<0 || destin<0)
-		{
-			printf("Invalid edge!\n");
-			count--;
-		}
-		else
-		{
-			adj[origin][destin] = 1;
-		}
+	for (i=1;i<=n;i++) {
+		q[i]=0;
+		visited[i]=0;
 	}
+	printf("\n Enter graph data in matrix form:\n");
+	for (i=1;i<=n;i++)
+	  for (j=1;j<=n;j++)
+	   scanf("%d",&a[i][j]);
+	printf("\n Enter the starting vertex:");
+	scanf("%d",&v);
+	bfs(v);
+	printf("\n The node which are reachable are:\n");
+	for (i=1;i<=n;i++)
+	  if(visited[i])
+	   printf("%d\t",i); else
+	   printf("\n here Bfs is not possible");
+	getch();
 }
